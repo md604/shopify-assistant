3ae1c32a9a72d01097cd784af20462bea3ef4ed5
Maxim Dyuzhinov <mdyuzhinov@gmail.com>
2022-01-24 23:17:27 +0100

    test local storage API with a service worker

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,20 @@
+const filter = {
+    url: [
+        {
+            urlMatches: 'https://gerarddarel.com/products/pull-gilet-douce_5718',
+        },
+    ],
+};
+
+chrome.webNavigation.onCompleted.addListener(() => {
+    console.info("The user has loaded my favorite website!");
+    fetch('https://gerarddarel.com/products/pull-gilet-douce_5718.json')
+    .then(data => data.json())
+    .then(data => {
+        chrome.storage.local.set({ product: data.product }, function() {
+            console.log('Product object has been saved to a local storage: ', data.product); 
+        });
+    })
+}, filter);
+
 export {}
