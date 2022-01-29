d12f6648e96f9cb15094a030655cdc0812662097
Maxim Dyuzhinov <mdyuzhinov@gmail.com>
2022-01-30 00:00:44 +0100

    Add url filter for shopify admin pages

--- src/background.ts
+++ src/background.ts
@@ -1,13 +1,15 @@
 const filter = {
     url: [
         {
-            urlMatches: 'https://gerarddarel.com/products/pull-gilet-douce_5718',
+            urlMatches: 'https://.*myshopify.com/admin'
         },
     ],
 };
 
-chrome.webNavigation.onCompleted.addListener(() => {
-    console.info("The user has loaded my favorite website!");
+chrome.webNavigation.onCompleted.addListener((details) => {
+    const themesUrl = `${details.url.split('admin')[0]}admin/themes.json`;
+    console.info("The user has loaded my favorite website!", details.url, themesUrl);
+    /*
     fetch('https://gerarddarel.com/products/pull-gilet-douce_5718.json')
     .then(data => data.json())
     .then(data => {
@@ -15,6 +17,7 @@ chrome.webNavigation.onCompleted.addListener(() => {
             console.log('Product object has been saved to a local storage: ', data.product); 
         });
     })
+    */
 }, filter);
 
 export {}
