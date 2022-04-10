ec3025ee65d19219043ea2edf719d37a24569b50
Maxim Dyuzhinov <mdyuzhinov@gmail.com>
2022-04-10 16:59:26 +0200

    Create zip bundles based on a manifest version

--- task_scripts/create_zip.ts
+++ task_scripts/create_zip.ts
@@ -1,3 +1,28 @@
-const child_process = require("child_process");
-child_process.execSync(`zip -j ./zips/${process.env.npm_package_version}.zip ./dist/*`, {stdio: 'inherit'});
+//const child_process = require("child_process");
+//child_process.execSync(`zip -j ./zips/${process.env.npm_package_version}.zip ./dist/*`, {stdio: 'inherit'});
+
+const AdmZip = require('adm-zip');
+const path = require('path');
+const fs = require('fs');
+
+const directoryPath = path.resolve('dist');
+
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    const zip = new AdmZip();
+    let zipName: string = 'no_version';
+
+    files.forEach(function (file) {
+        if (file == 'manifest.json') {            
+            const manifestData = fs.readFileSync(path.join('./dist/',file));
+            const manifestJSON = JSON.parse(manifestData);
+            zipName = manifestJSON.version;
+        }
+        zip.addLocalFile(path.join('./dist/',file));
+    });
+    zip.writeZip(path.join('./zips/',`v${zipName}.zip`));
+});
+
 export {};
